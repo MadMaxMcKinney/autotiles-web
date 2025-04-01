971eda19dce74b980e8454746b3b64e494a4c29f
Max McKinney <maxgames101@gmail.com>
2025-04-01 17:15:48 -0500

    init

---
+++ src/app/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen w-full">
+            <h1 className="text-4xl font-bold">Welcome to Autotiles!</h1>
+            <p className="mt-4 text-lg">A collection of modular automotive tools and resources, all in one place.</p>
+        </div>
+    );
+}
